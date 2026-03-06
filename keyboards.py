4ed7118c2374b0fd6d79c8d24d0612e75289dfae
adnan129798a <adnan.129798a@gmail.com>
2026-03-06 23:30:26 +0100

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -8,15 +8,17 @@ def menu(lang):
             [TEXTS[lang]["content_ideas"], TEXTS[lang]["captions"]],
             [TEXTS[lang]["hashtags"], TEXTS[lang]["video_script"]],
             [TEXTS[lang]["image_ai"], TEXTS[lang]["video_ai"]],
-            [TEXTS[lang]["invite"], TEXTS[lang]["my_ref"]]
+            [TEXTS[lang]["invite"], TEXTS[lang]["my_ref"]],
         ],
         resize_keyboard=True
     )
 
 
 def unlock_keyboard(lang, channel_url, invite_link):
+    share_url = f"https://t.me/share/url?url={invite_link}&text=جرب هذا البوت الذكي"
+
     return InlineKeyboardMarkup([
         [InlineKeyboardButton(TEXTS[lang]["join_channel"], url=channel_url)],
-        [InlineKeyboardButton(TEXTS[lang]["share_invite"], url=invite_link)],
+        [InlineKeyboardButton(TEXTS[lang]["share_invite"], url=share_url)],
         [InlineKeyboardButton(TEXTS[lang]["check_unlock"], callback_data="check_unlock")]
     ])
