480920a5a3971576b53e5de6fe2acc1c778e4414
adnan129798a <adnan.129798a@gmail.com>
2026-03-07 00:30:24 +0100

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -22,3 +22,12 @@ def unlock_keyboard(lang, channel_url, invite_link):
         [InlineKeyboardButton(TEXTS[lang]["share_invite"], url=share_url)],
         [InlineKeyboardButton(TEXTS[lang]["check_unlock"], callback_data="check_unlock")]
     ])
+
+
+def image_style_keyboard(lang):
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton(TEXTS[lang]["more_realistic"], callback_data="img_hyper_real")],
+        [InlineKeyboardButton(TEXTS[lang]["anime_style"], callback_data="img_anime")],
+        [InlineKeyboardButton(TEXTS[lang]["cinematic_style"], callback_data="img_cinematic")],
+        [InlineKeyboardButton(TEXTS[lang]["regenerate"], callback_data="img_regenerate")]
+    ])
