afbf8768ad5281c6ca267a50e06c7c1d753145b9
adnan129798a <adnan.129798a@gmail.com>
2026-03-07 00:27:28 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL")
 REQUIRED_CHANNEL_URL = os.getenv("REQUIRED_CHANNEL_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing.")
@@ -12,3 +13,6 @@ if not REQUIRED_CHANNEL:
 
 if not REQUIRED_CHANNEL_URL:
     raise ValueError("REQUIRED_CHANNEL_URL is missing.")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY is missing.")
